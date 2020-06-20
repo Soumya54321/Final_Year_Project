3377d996e89837edca21a158d594acbea2843c4d
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2020-06-20 22:03:57 +0530

    Submission of new book is done

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, redirect, request, jsonify, url_for
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = "./images"
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/')
+def index():
+    return "Bye"
+
+
+@app.route('/Admin', methods = ["GET"])
+def admin():
+    if request.method == "GET":
+        return render_template('/Admin_Pannel/Admin.html')
+
+@app.route('/done')
+def done():
+    # if request.method == "GET":
+    return render_template('/Admin_Pannel/extra.html', success = request.args.get("stat"))
+
+
+@app.route('/admin_save', methods = ["POST", "GET"])
+def admin_save():
+    if request.method == "POST":
+        print("Hello")
+        userData = request.form
+        file = request.files['file']
+        
+        print(userData)
+        if file:
+            print(file)
+            # filename = secure_filename(file.filename)
+
+            for f in request.files.getlist('file'):
+                filename = secure_filename(f.filename)
+                f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        
+        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        stat = "Done..."
+        return redirect(url_for('done', stat = stat))
+
+if __name__ == "__main__":
+    app.run(debug = True)
