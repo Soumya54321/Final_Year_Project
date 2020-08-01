ef1d2e3debaece3f7b57e6a4049e82d08212ba2e
Soumyadeep Paul <soumyadeeppaul0@gmail.com>
2020-08-01 14:40:33 +0530

    Comments are added + Database is added

--- app.py
+++ app.py
@@ -1,54 +1,88 @@
+#Add flask packages
 from flask import Flask, render_template, redirect, request, jsonify, url_for
+from flaskext.mysql import MySQL
 from werkzeug.utils import secure_filename
 import os
 
-UPLOAD_FOLDER = './books'
+# ******************************************************** 
+# Configuration part
+# ********************************************************
 
+#Initialize the flask app
 app = Flask(__name__)
+
+#Configure the upload folder for storing pdfs
+UPLOAD_FOLDER = './books'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
-    return "Bye"
+
+#Configure the MySQL database
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'db password'
+app.config['MYSQL_DATABASE_DB'] = 'OurLibrary'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+#Check if DB is connected or not
+if cursor: 
+    print("Congrats!! Database is connected!!")
+else:
+    print("Oops!! Database is not connected!!")
 
 
-@app.route('/Admin', methods = ["GET"])
+
+# ****************************************
+# Routes to admin panel
+# ****************************************
+
+#Route for admin dashboard
+@app.route('/admin/dashboard', methods = ["GET"])
 def admin():
     if request.method == "GET":
-        return render_template('/Admin_Pannel/Admin.html')
-
-@app.route('/done')
-def done():
-    # if request.method == "GET":
-    return render_template('/Admin_Pannel/extra.html', success = request.args.get("stat"))
+        return render_template('/adminPanel/admin.html')
 
 
-@app.route('/admin_save', methods = ["POST", "GET"])
+#Route for saving any admin uploaded pdf
+@app.route('/admin/save_pdf', methods = ["POST", "GET"])
 def admin_save():
     if request.method == "POST":
-        print("Hello")
         userData = request.form
         file = request.files['file']
-        
         print(userData)
+        
+        #If there is any file
         if file:
-            print(file)
-            # filename = secure_filename(file.filename)
-
             for f in request.files.getlist('file'):
                 filename = secure_filename(f.filename)
                 f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            
-        # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
         stat = "Done..."
         return redirect(url_for('done', stat = stat))
+    elif request.method == "GET":
+        return render_template('/adminPanel/error.html')
 
 
 
+# *****************************************
+# Default Routes
+# *****************************************
+
+#Route for testing done
+@app.route('/done')
+def done():
+    # if request.method == "GET":
+    return render_template('/adminPanel/extra.html', success = request.args.get("stat"))
+
+#Route for testing purpose
 @app.route('/test',methods=["POST","GET"])
 def testing():
-    return render_template('/Admin_Pannel/error.html')
+    return render_template('/adminPanel/error.html')
 
 @app.route('/signup',methods=["POST","GET"])
 def registration():
